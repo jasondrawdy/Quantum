77bb5dbd7b9698e7059e78535a2cfdd3807c4dae
Jason Drawdy <40871836+jasondrawdy@users.noreply.github.com>
2020-05-06 16:39:58 -0400

    Update quantum_measurement_ibmq.py

--- quantum_measurement_ibmq.py
+++ quantum_measurement_ibmq.py
@@ -1,3 +1,11 @@
+"""
+Program: quantum_measurement_ibmq.py
+Author: Jason Drawdy
+Date: May 6th, 2020 (05.06.20)
+Description:
+This program simulates the measurement of a quantum bit called a qubit using the ProjectQ API.
+"""
+
 import os
 from pathlib import Path
 
