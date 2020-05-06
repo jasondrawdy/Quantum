d141df2fafd5bb97cabb1fa9f8ca16784a6cbe7f
Jason Drawdy <40871836+jasondrawdy@users.noreply.github.com>
2020-05-06 16:41:00 -0400

    Update quantum_number_generator.py

--- quantum_number_generator.py
+++ quantum_number_generator.py
@@ -3,7 +3,7 @@ Program: quantum_number_generator.py
 Author: Jason Drawdy
 Date: May 6th, 2020 (05.06.20)
 Description:
-This program simulates the measurement of a quantum bit called a qubit using the ProjectQ API.
+This program generates a set of random numbers using the ProjectQ API.
 """
 
 from projectq.ops import H, Measure
